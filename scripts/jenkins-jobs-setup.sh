d8e58810a003e774aa06ea863eb3d2c3eb26699e
tushar5526 <codingid6@gmail.com>
2022-11-30 17:23:56 +0530

    Move db pipelines to home

--- scripts/jenkins-jobs-setup.sh
+++ scripts/jenkins-jobs-setup.sh
@@ -5,7 +5,7 @@ echo -e "\n\e[0;32m${bold}Removing /var/lib/jenkins/jobs ${normal}\n"
 rm -rf /var/lib/jenkins/jobs
 
 echo -e "\n\e[0;32m${bold}Making a symlink where /var/lib/jenkins/jobs points to /home/samarth-devops/jenkins-jobs/jobs ${normal}\n"
-ln -s /home/samarth-devops/jenkins-jobs/jobs/ /var/lib/jenkins/
+ln -s /home/samarth-devops/jenkins-jobs/jobs/Samarth /var/lib/jenkins/jobs
 
 echo -e "\n\e[0;32m${bold}Chown the jobs to jenkins user ${normal}\n"
 chown -R jenkins:jenkins /var/lib/jenkins/jobs
